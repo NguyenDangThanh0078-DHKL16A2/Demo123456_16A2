3913dfb57c0e81820d66bf137c976809d2e953e3
Khoa hoc du lieu 16A2 <ndthanh.dhkl16a2hn@sv.uneti.edu.vn>
2022-11-30 16:34:51 +0700

    lam bai tap

--- C08/baitap8.17.py
+++ C08/baitap8.17.py
@@ -1,2 +1,13 @@
-x=6%4
-print(x)
+#BCNN
+a = int(input("Nhập a: "))
+b = int(input("Nhập b: "))
+x = min(a,b)
+y = max(a,b)
+k = 1
+i = 1
+while k!=0:
+    if (x*i)%y ==0:
+        k = 0
+        print("BCNN=",x*i)
+    else:
+        i+=1
