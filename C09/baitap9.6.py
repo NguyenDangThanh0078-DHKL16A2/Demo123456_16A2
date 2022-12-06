acbe20e99ccc9afb4b141b544966ec4fe899327f
Khoa hoc du lieu 16A2 <ndthanh.dhkl16a2hn@sv.uneti.edu.vn>
2022-12-07 00:49:56 +0700

    húp

---
+++ C09/baitap9.6.py
@@ -0,0 +1,17 @@
+#Kiểm tra số nguyên tố
+n = int(input('nhập số nguyên:'))
+def kiem_tra_so_nguyen_to(n):
+    flag = True
+    if n<2:
+    	flag = False
+    elif n==2:
+    	flag = True
+    elif n%2==0:
+    	flag = False
+    else:
+    	for i in range(3,n,2):
+    		if n%i==0:
+    		    flag = False
+    return flag
+print(kiem_tra_so_nguyen_to(n))
+    
